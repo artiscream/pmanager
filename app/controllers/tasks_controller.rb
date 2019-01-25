37cf798b6f3bcacb5a908568de86298316764132
artiscream <arti_scream@ukr.net>
2019-01-25 13:34:12 +0200

    some logic with task

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,40 @@
+class TasksController < ApplicationController
+	before_action :find_project
+	before_action :find_task, only: [:destroy, :complete]
+
+	def create
+		@task = @project.tasks.create(task_params)
+
+		redirect_to @project
+	end
+
+	def destroy
+
+		if @task.destroy
+			flash[:success] = "Task was deleted"
+		else
+			flash[:error] = "Cannot delete task"
+		end
+
+		redirect_to @project
+	end
+
+	def complete
+		@task.update_attribute(:completed_at, Time.now)
+		redirect_to @project. notice: "Task completed!"
+	end
+
+	private
+
+		def find_project
+			@project = Project.find(params[:project_id])
+		end
+
+		def find_task
+			@task = @project.tasks.find(params[:id])
+		end
+
+		def task_params
+			params[:task].permit(:content)
+		end
+end
